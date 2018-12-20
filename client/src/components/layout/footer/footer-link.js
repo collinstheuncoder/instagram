48691c8a7fbbbb75e95d64614cf2f7b0e5980b48
collinstheuncoder <collinstheuncoder@gmail.com>
2018-12-20 18:57:33 +0000

    completed small screens footer ui

---
+++ client/src/components/layout/footer/footer-link.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { object } from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Grid, Icon } from 'semantic-ui-react';
+
+import { column, link, icon } from './footer.module.scss';
+
+function FooterLink({ footerLink }) {  
+  return (
+    <Grid.Column className={column}>
+      <Link to={footerLink.path} className={link}>
+        <Icon name={footerLink.icon} size="large" className={icon} />
+      </Link>
+    </Grid.Column>
+  );
+}
+
+FooterLink.propTypes = {
+  footerLink: object.isRequired,
+};
+
+export default FooterLink;
--- client/src/components/layout/footer/index.js
+++ client/src/components/layout/footer/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+
+import FooterLink from './footer-link';
+
+import { footer, grid, row } from './footer.module.scss';
+
+const footerLinks = [
+  { icon: 'home', path: '/' },
+  { icon: 'search', path: '/' },
+  { icon: 'plus square outline', path: '/' },
+  { icon: 'heart outline', path: '/' },
+  { icon: 'user outline', path: '/account/profile' },
+];
 
 function InstaFooter() {
   return (
-    <footer className="footer">
-      Footer
+    <footer className={footer}>
+      <Grid columns={5} className={grid}>
+        <Grid.Row className={row}>
+          {footerLinks.map((footerLink, index) => (
+            <FooterLink key={index} footerLink={footerLink} />
+          ))}
+        </Grid.Row>
+      </Grid>
     </footer>
   );
 }

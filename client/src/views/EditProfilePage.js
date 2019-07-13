93c1d0886c34d49506a9a864480a1bc8410cca15
Collins Orlando <collinstheuncoder@gmail.com>
2019-07-13 17:29:29 +0100

    clean up views directory

--- client/src/views/EditProfilePage.js
+++ client/src/views/EditProfilePage.js
@@ -1,11 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
+import { func, object } from 'prop-types';
+import { connect } from 'react-redux';
+import { Card, Grid } from 'semantic-ui-react';
 
-function EditProfilePage() {
-  return (
-    <div>
-      Edit Profile AuthPage
-    </div>
-  );
+import GeneralInfo from './edit-account/GeneralInfo';
+import ChangePassword from './edit-account/ChangePassword';
+
+import style from './editprofile-page.module.scss';
+
+import {
+  fetchCurrentUser, 
+  updateUserInfo,
+  changePassword,
+  deleteAccount,
+} from '../containers/users/actions';
+
+class EditProfilePage extends Component {
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (token) this.props.fetchCurrentUser(token);
+  }
+
+  render() {
+    const { currentUser, updateUserInfo, deleteAccount } = this.props;
+    const { card, grid, link, row } = style;
+
+    return (
+      <Grid className={grid}>
+        <Card className={card}>
+          <Grid.Row className={row} columns={2} divided>
+            <Grid.Column className={style['column-left']}>
+              <NavLink
+                className={link}
+                activeStyle={{
+                  borderLeft: '0.125rem solid #333333',
+                  fontWeight: 600,
+                  backgroundColor: '#fafafa',
+                }}
+                to="/accounts/edit"
+                exact
+              >
+                Edit Profile
+              </NavLink>
+              <NavLink
+                className={link}
+                activeStyle={{
+                  borderLeft: '0.125rem solid #333333',
+                  fontWeight: 600,
+                  backgroundColor: '#fafafa',
+                }}
+                to="/accounts/edit/change-password"
+              >
+                Change Password
+              </NavLink>
+            </Grid.Column>
+            <Grid.Column className={style['column-right']} width={12}>
+              <Switch>
+                <Route
+                  path="/accounts/edit"
+                  exact
+                  component={() => (
+                    <GeneralInfo
+                      updateUserInfo={updateUserInfo}
+                      deleteAccount={deleteAccount}
+                      currentUser={currentUser}
+                    />
+                  )}
+                />
+                <Route
+                  path="/accounts/edit/change-password"
+                  component={() => (
+                    <ChangePassword
+                      changePassword={changePassword}
+                      currentUser={currentUser}
+                    />
+                  )}
+                />
+              </Switch>
+            </Grid.Column>
+          </Grid.Row>
+        </Card>
+      </Grid>
+    );
+  }
 }
 
-export default EditProfilePage;
+EditProfilePage.propTypes = {
+  currentUser: object.isRequired,
+  fetchCurrentUser: func.isRequired,
+  updateUserInfo: func.isRequired,
+  changePassword: func.isRequired,
+  deleteAccount: func.isRequired,
+};
+
+function mapStateToProps({ users }) {
+  return {
+    currentUser: users.currentUser,
+    error: users.error,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchCurrentUser, updateUserInfo, changePassword, deleteAccount }
+)(EditProfilePage);
--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
 import InstaBgImg from '../components/landing/InstaBgImg';
 import InstaAuth from '../components/landing/InstaAuth';
 
+import { createAccount, loginUser } from '../containers/auth/actions';
+
 import style from './landing-page.module.scss';
 
-function LandingPage() {
+function LandingPage(props) {
   return (
     <Grid className={style.grid}>
       <Grid.Row className={style.row} columns={2}>
         <Grid.Column className={`${style.column} ${style['column--bg-img']}`}>
-          <InstaBgImg />
+          <InstaBgImg {...props}  />
         </Grid.Column>
         <Grid.Column className={`${style.column} ${style['column--auth']}`}>
-          <InstaAuth />
+          <InstaAuth {...props} />
         </Grid.Column>
       </Grid.Row>
     </Grid>
   );
 }
 
-export default LandingPage;
+function mapStateToProps({ auth }) {
+  return {
+    isLoggedIn: auth.isLoggedIn,
+    isLoading: auth.isLoading,
+    error: auth.error,
+  };
+}
+
+export default connect(mapStateToProps, { createAccount, loginUser })(LandingPage);
+ 
